3518cb4e97e7e08f6f225f07efecbf15e7798573
Efrain Rico Galvan <erico21@alumnos.uaq.mx>
2025-07-19 09:50:53 -0600

    pagina completa

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,6 +2,7 @@ import tailwind from "@astrojs/tailwind";
 import { defineConfig } from "astro/config";
 
 export default defineConfig({
+  evToolbar: false,
   integrations: [tailwind()],
   image: {
     domains: [
--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -4,7 +4,20 @@ module.exports = {
     extend: {
       colors: {
         truper: "#f37021",
-        
+        spanish: '#F0660D',
+        persian: '#E0926B',
+        onyx: '#454545',
+        snow: '#FFF5F5',
+        brown: '#9A3B12',
+      },
+      animation: {
+        marquee: "marquee 30s linear infinite",
+      },
+      keyframes: {
+        marquee: {
+          "0%": { transform: "translateX(0%)" },
+          "100%": { transform: "translateX(-50%)" },
+        },
       },
     },
   },
